123576645ad17107ff57d181dc4133ef2886f741
San Kim <waterfog9580@gmail.com>
2023-05-24 13:56:21 +0900

    :sparkles: add jwt configure

--- admin-api/app/common/config.py
+++ admin-api/app/common/config.py
@@ -2,15 +2,34 @@ from pydantic import BaseSettings
 from app.common.constants import Environment
 
 
+class Config:
+    env_file = '.env'
+    env_file_encoding = 'utf-8'
+
+
 class Settings(BaseSettings):
+    class Config(Config):
+        pass
+
     CORS_ORIGINS: list[str]
     CORS_HEADERS: list[str]
 
     ENVIRONMENT: Environment = Environment.LOCAL
 
-    class Config:
-        env_file = '.env'
-        env_file_encoding = 'utf-8'
+
+class AuthConfig(BaseSettings):
+    class Config(Config):
+        pass
+
+    JWT_ALG: str
+    JWT_SECRET: str
+    JWT_EXP: int = 5  # minutes
+
+    REFRESH_TOKEN_KEY: str = "refreshToken"
+    REFRESH_TOKEN_EXP: int = 60 * 60 * 24 * 21  # 21 days
+
+    SECURE_COOKIES: bool = True
 
 
 settings = Settings()
+auth_config = AuthConfig()
